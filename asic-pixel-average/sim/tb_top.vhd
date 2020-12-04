9e16fdc11bd37a946bf9b6454a4f919bb0629589
Andre Mattos <andrempmattos@gmail.com>
2020-12-04 17:19:06 -0300

    asic-pixel-average: sim: Adding external ram testbench

---
+++ asic-pixel-average/sim/tb_top.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+entity top is
+end entity;
+
+architecture behavioral of top is
+
+    
+
+end architecture;
